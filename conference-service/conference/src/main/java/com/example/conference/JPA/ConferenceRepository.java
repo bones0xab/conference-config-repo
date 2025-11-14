1bd3361495b5d1ba3eb3387dc7dc87bbee1f69d9
0x_beast <117541618+bones0xab@users.noreply.github.com>
2025-11-14 11:37:24 +0100

    Delete conference-service directory

--- conference-service/conference/src/main/java/com/example/conference/JPA/ConferenceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.conference.JPA;
-
-import com.example.conference.entities.Conference;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConferenceRepository extends JpaRepository<Conference, Long> {
-}
--- conference-service/conference/src/main/java/com/example/conference/enums/ConferenceType.java
+++
@@ -1,6 +0,0 @@
-package com.example.conference.enums;
-
-public enum ConferenceType {
-    ACADEMIQUE,
-    COMMERCIALE
-}
